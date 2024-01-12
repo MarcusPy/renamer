440c1b61625251ba22e2e8722619ded39bce33e5
MarcusPy <107936046+MarcusPy@users.noreply.github.com>
2024-01-12 02:19:32 +0100

    release 1.0
    Major improvements in every aspect

--- renamer.py
+++ renamer.py
@@ -1,56 +1,134 @@
 import os
+from sys import exit as abort
+os.system("cls")
 
 """
-mode
-1 - add prefix, add index
-2 - add prefix, keep original name
-3 - add prefix, keep original name, add index
-4 - add custom name, add index
-5 - add prefix, add custom name, add index
-6 - trim first found prefix
+Available modes:
+    1 - Prefix + Index
+        Required:
+            prefix
+        Optional:
+            sort_by_created_date
+            sort_by_modified_date
+            
+    2 - Original Name + Index
+        Required:
+            separators
+        Optional:
+            sort_by_created_date
+            sort_by_modified_date
+            
+    3 - Prefix + Original Name
+        Required:
+            prefix
+        Optional:
+            sort_by_created_date
+            sort_by_modified_date
+            trim_all_prefixes (Recommended, but read the comment first)
+            
+    4 - Prefix + Original Name + Index
+        Required:
+            prefix
+            separators
+        Optional:
+            sort_by_created_date
+            sort_by_modified_date
+            trim_all_prefixes (Recommended, but read the comment first)
+            
+    5 - Custom Name + Index
+        Required:
+            custom_name
+            separators
+        Optional:
+            sort_by_created_date
+            sort_by_modified_date
+            
+    6 - Prefix + Custom Name + Index
+        Required:
+            prefix
+            custom_name
+            separators
+        Optional:
+            sort_by_created_date
+            sort_by_modified_date
+            trim_all_prefixes (Recommended, but read the comment first)
 """
 
-mode = 1
-prefix = 'f'
-custom_name = 'pic'
-sort_by_created_date = True
-sort_by_modified_date = False
-allowed_formats = [ '.mp4', '.mov', '.avi', '.png', '.jpg' ]
+MODE: int = 4
+PREFIX: str = 'f'
+CUSTOM_NAME: str = 'pic'
+SORT_BY_CREATED_DATE: bool = False
+SORT_BY_MODIFIED_DATE: bool = False
+ALLOWED_FORMATS: list[str] = [ '.mp4', '.mov', '.avi', '.png', '.jpg' ]
+# will remove every index that follows this template, until reaches <other>:
+# <other><separator><number>[...]<separator><number>
+SEPARATORS: list[str] = [ ' ', '_' ]
+# will remove every prefix that follows this template:
+# <any>_[...]<any>_
+# so make sure your name does not contain any underscores _
+# setting this to False means only 1 prefix will be removed
+# if you wish to leave them untouched, choose a mode without 'prefix' 
+TRIM_ALL_PREFIXES: bool = True
 
-if (
-    type(mode) is not int
-    or
-    type(prefix) is not str
-    or
-    type(custom_name) is not str
-    or
-    type(sort_by_created_date) is not bool
-    or
-    type(sort_by_modified_date) is not bool
-    or
-    (
-        sort_by_created_date == True
-        and 
-        sort_by_modified_date == True
-    )
-    or
-    prefix == ''
-    or
-    custom_name == ''
-):
-    exit()
+#######################################################################################
+
+REVISION = [
+    '12/I/2024',
+    'v1.0'
+]
+
+errors = [param for param, condition in zip(
+    [
+        'Parametre [MODE] must be a string',
+        'Parametre [PREFIX] must be a string',
+        'Parametre [PREFIX] must not be empty',
+        'Parametre [CUSTOM_NAME] must be a string',
+        'Parametre [CUSTOM_NAME] must not be empty',
+        'Parametre [SORT_BY_CREATED_DATE] must be a bool',
+        'Parametre [SORT_BY_MODIFIED_DATE] must be a bool',
+        'Parametres [SORT_BY_CREATED_DATE] and [SORT_BY_MODIFIED_DATE] must not be enabled at the same time',
+        'Parametre [ALLOWED_FORMATS] must be a list',
+        'Parametre [ALLOWED_FORMATS] must not be empty',
+        'Parametre [ALLOWED_FORMATS] must contain only strings',
+        'Parametre [SEPARATORS] must be a list',
+        'Parametre [SEPARATORS] must not be empty',
+        'Parametre [SEPARATORS] must contain only strings',
+        'Parametre [TRIM_ALL_PREFIXES] must be a bool'
+    ],
+    [
+        isinstance(MODE, int),
+        isinstance(PREFIX, str),
+        PREFIX != '',
+        isinstance(CUSTOM_NAME, str),
+        CUSTOM_NAME != '',
+        isinstance(SORT_BY_CREATED_DATE, bool),
+        isinstance(SORT_BY_MODIFIED_DATE, bool),
+        not (SORT_BY_CREATED_DATE and SORT_BY_MODIFIED_DATE),
+        isinstance(ALLOWED_FORMATS, list),
+        len(ALLOWED_FORMATS) > 0,
+        all(isinstance(elem, str) for elem in ALLOWED_FORMATS),
+        isinstance(SEPARATORS, list),
+        len(SEPARATORS) > 0,
+        all(isinstance(elem, str) for elem in SEPARATORS),
+        isinstance(TRIM_ALL_PREFIXES, bool)
+    ]
+) if not condition]
+
+if errors:
+    for error in errors:
+        print(error)
+    abort('Process aborted due to invalid config')
 else:
-    if sort_by_created_date == True:
+    if SORT_BY_CREATED_DATE == True:
         sort_mode = 1
-    elif sort_by_modified_date == True:
+    elif SORT_BY_MODIFIED_DATE == True:
         sort_mode = 2
     else:
         sort_mode = 0
 
-def sort_by_x_date(enabled:'int'=0) -> 'list':
+def sort_by_x_date(enabled:int=0) -> list:
+    files = [file for file in os.listdir() for ext in ALLOWED_FORMATS if ext in file]
     if enabled > 0:
-        files = [file for file in os.listdir()]
-        
         if enabled == 1:
             files.sort(key=os.path.getctime)
         elif enabled == 2:
@@ -58,73 +136,111 @@ def sort_by_x_date(enabled:'int'=0) -> 'list':
             
         return files
     elif enabled == 0:
-        return os.listdir()
+        return files
 
-def trim_prefix():
-    success, total = 0, len(os.listdir()) - 1
-    skipped = []
+def trimmer(file_list:list=[], trim_idx:bool=True, trim_pre:bool=True) -> list:
+    done, nums = [], [ str(num) for num in range(10) ]
+    for file in file_list:
+        name, _ = os.path.splitext(file)
+        name = name[::-1]
+        
+        if trim_idx and (name[0] in nums) or (name[0] in SEPARATORS):
+            to_cut, last_separator_idx = 0, 0
+            last_char = None
+            for idx, char in enumerate(name):
+                if char in nums:
+                    to_cut += 1
+                    last_char = int
+                elif char in SEPARATORS:
+                    to_cut += 1
+                    last_char = str
+                    last_separator_idx = idx
+                else:
+                    if last_char is str:
+                        name = name[to_cut:]
+                        break
+                    elif last_char is int and last_separator_idx > 0:
+                        name = name[last_separator_idx+1:]
+                        break
+                    else:
+                        break
+        
+        name = name[::-1]
+        
+        if trim_pre:
+            for _ in range(1):
+                pos = name.find('_')
+                if pos != -1:
+                    name = name[pos+1:]
+                while TRIM_ALL_PREFIXES:
+                    pos = name.find('_')
+                    if pos != -1:
+                        name = name[pos+1:]
+                    else:
+                        break
+
+        done.append(name)
+    
+    unique = {}
+    for name in done:
+        if unique.get(name) is None:
+            unique[name] = 1
+
+    return done, unique
+
+def trim_temp():
     for file in os.listdir():
-        name, ext = os.path.splitext(file)
-        pos = name.find('_')
-        if pos == -1 or ext.lower() not in allowed_formats:
-            skipped.append(''.join(name + ext))
-            continue
-        success+=1
-        name = name[pos+1:]
-        new_name = f'{name}{ext.lower()}'
-        os.rename(file, new_name)
-        
-    print(f'[{success}/{total}] trimmed\nSkipped: {skipped}')
-        
-if mode == 1:
-    trim_prefix()
+        name = file.replace('temp_', '')
+        os.rename(file, name)
+
+if MODE == 1:
     files = sort_by_x_date(sort_mode)
     for idx, file in enumerate(files):
         _, ext = os.path.splitext(file)
-        if ext.lower() not in allowed_formats:
-            continue
-        new_name = f'{prefix}_{idx+1}{ext.lower()}'
+        new_name = f'temp_{PREFIX}_{idx+1}{ext.lower()}'
         os.rename(file, new_name)
 
-elif mode == 2:
-    trim_prefix()
+elif MODE == 2:
     files = sort_by_x_date(sort_mode)
-    for file in files:
-        name, ext = os.path.splitext(file)
-        if ext.lower() not in allowed_formats:
-            continue
-        new_name = f'{prefix}_{name}{ext.lower()}'
-        os.rename(file, new_name)
-
-elif mode == 3:
-    trim_prefix()
-    files = sort_by_x_date(sort_mode)
+    names, unique = trimmer(files, trim_pre=False)
     for idx, file in enumerate(files):
-        name, ext = os.path.splitext(file)
-        if ext.lower() not in allowed_formats:
-            continue
-        new_name = f'{prefix}_{name}_{idx+1}{ext.lower()}'
+        _, ext = os.path.splitext(file)
+        val = unique.get(names[idx])
+        unique[names[idx]] += 1
+        new_name = f'temp_{names[idx]} {val}{ext.lower()}'
         os.rename(file, new_name)
 
-elif mode == 4:
-    trim_prefix()
+elif MODE == 3:
+    files = sort_by_x_date(sort_mode)
+    names, unique = trimmer(files, False)
+    for idx, file in enumerate(files):
+        _, ext = os.path.splitext(file)
+        new_name = f'temp_{PREFIX}_{names[idx]}{ext.lower()}'
+        os.rename(file, new_name)
+
+elif MODE == 4:
+    files = sort_by_x_date(sort_mode)
+    names, unique = trimmer(files)
+    for idx, file in enumerate(files):
+        _, ext = os.path.splitext(file)
+        val = unique.get(names[idx])
+        unique[names[idx]] += 1
+        #print(f'NAME: {names[idx]} | USED: {val}')
+        new_name = f'temp_{PREFIX}_{names[idx]} {val}{ext.lower()}'
+        os.rename(file, new_name)
+
+elif MODE == 5:
     files = sort_by_x_date(sort_mode)
     for idx, file in enumerate(files):
         _, ext = os.path.splitext(file)
-        if ext.lower() not in allowed_formats:
-            continue
-        new_name = f'{custom_name}_{idx+1}{ext.lower()}'
+        new_name = f'{CUSTOM_NAME} {idx+1}{ext.lower()}'
         os.rename(file, new_name)
 
-elif mode == 5:
-    trim_prefix()
+elif MODE == 6:
     files = sort_by_x_date(sort_mode)
     for idx, file in enumerate(files):
         _, ext = os.path.splitext(file)
-        if ext.lower() not in allowed_formats:
-            continue
-        new_name = f'{prefix}_{custom_name}_{idx+1}{ext.lower()}'
+        new_name = f'{PREFIX}_{CUSTOM_NAME} {idx+1}{ext.lower()}'
         os.rename(file, new_name)
 
-elif mode == 6:
-    trim_prefix()
+trim_temp()
